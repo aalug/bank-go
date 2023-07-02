b85e9e89aaa602d38be8164d8283b47eceda1c23
Adam Gulczynski <adamg@example.com>
2023-07-02 12:04:31 +0200

    separate the Dockerfile and the main.go for gin and gRPC (#47)

---
+++ gapi/server.go
@@ -0,0 +1,32 @@
+package gapi
+
+import (
+	"fmt"
+	db "github.com/aalug/go-bank/db/sqlc"
+	"github.com/aalug/go-bank/pb"
+	"github.com/aalug/go-bank/token"
+	"github.com/aalug/go-bank/utils"
+)
+
+// Server serves gRPC requests for the service
+type Server struct {
+	pb.UnimplementedGoBankServer
+	config     utils.Config
+	store      db.Store
+	tokenMaker token.Maker
+}
+
+// NewServer creates a new gRPC server
+func NewServer(config utils.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker:  %w", err)
+	}
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
+	return server, nil
+}
--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"database/sql"
 	"github.com/aalug/go-bank/api"
 	db "github.com/aalug/go-bank/db/sqlc"
+	"github.com/aalug/go-bank/gapi"
+	"github.com/aalug/go-bank/pb"
 	"github.com/aalug/go-bank/utils"
 	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log"
+	"net"
+	"os"
 )
 
 func main() {
@@ -21,13 +27,45 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+
+	serverType := os.Getenv("SERVER_TYPE")
+	if serverType == "gin" {
+		runGinServer(config, store)
+	} else {
+		runGrpcServer(config, store)
+	}
+}
+func runGinServer(config utils.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("cannot create server: ", err)
 	}
 
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start the server:", err)
+	}
+}
+
+func runGrpcServer(config utils.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server: ", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGoBankServer(grpcServer, server)
+
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create a listener:", err)
+	}
+
+	log.Printf("gRPC server listening at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start the gRPC server:", err)
 	}
 }
